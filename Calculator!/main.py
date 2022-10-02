e50d30f1dff4a8bc95492b66cdde461b5fb081bf
Dogwaterzx <100448941+Dogwaterzx@users.noreply.github.com>
2022-10-02 18:13:44 -0400

    Create main.py

---
+++ Calculator!/main.py
@@ -0,0 +1,11 @@
+from helper import *
+
+# input: 2 numbers from user (operation)
+no1, no2, o = get_inputs()
+
+# process: add 2 numbers (use op later)
+
+result = calculate(no1, no2, o)
+
+# output: print result
+print(result)
